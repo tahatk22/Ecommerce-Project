d6cf3e55ba36b4daf361c5bd86b7674117ec3ba5
NANOSOFT <mohamedahmeds951@gmail.com>
2023-11-26 13:20:47 +0200

    x

---
+++ AttractDomain/Entities/Attract/ProductImage.cs
@@ -0,0 +1,15 @@
+﻿using Attract.Framework.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttractDomain.Entities.Attract
+{
+    public class ProductImage : EntityBase
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
