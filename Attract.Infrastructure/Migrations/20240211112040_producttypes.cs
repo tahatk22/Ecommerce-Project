a26a691256e3d89a4e50d98bdfc36b29518a2423
Mohamed Ahmed Saad <mohamedahmeds951@gmail.com>
2024-02-11 13:25:28 +0200

    changes

---
+++ Attract.Infrastructure/Migrations/20240211112040_producttypes.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Attract.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class producttypes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ProductTypeOption",
+                schema: "Attract",
+                table: "Product",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProductTypeOption",
+                schema: "Attract",
+                table: "Product");
+        }
+    }
+}
