591cdf6a8b6353a8f523097c3a5ce15548789acf
tahatk22 <tk2595922@gmail.com>
2024-03-13 20:32:33 +0200

    Using Swagger

--- Attract.API/Program.cs
+++ Attract.API/Program.cs
@@ -94,11 +94,11 @@ var app = builder.Build();
 app.UseCors("AllowAnyOrigin");
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 app.UseHttpsRedirection();
 
  app.UseStaticFiles();
